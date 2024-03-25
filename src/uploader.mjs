5e59fc67aa6269093960fd294e7392aca1633032
gfalco77 <giuseppefalco77@gmail.com>
2024-03-25 20:08:00 +0000

    fixed upload

--- src/uploader.mjs
+++ src/uploader.mjs
@@ -1,5 +1,5 @@
 import aws from 'aws-sdk';
-import Buffer from 'node:buffer';
+import multipart from 'lambda-multipart-parser';
 
 const s3 = new aws.S3();
 const BUCKET_NAME = 'vai-assets';
@@ -7,33 +7,29 @@ const BUCKET_NAME = 'vai-assets';
 export const lambdaHandler = async (event) => {
   const folder = 'unprocessed-images';
 
-  console.log(event);
-
-  const response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: JSON.stringify({message: "Successfully uploaded file to S3"}),
-  };
+  let body;
+  let statusCode = 200;
+  const result = await multipart.parse(event);
+  const { content, filename, contentType } = result.files[0];
 
   try {
-    const parsedBody = JSON.parse(event.body);
-    const base64File = parsedBody.file;
-    const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
     const params = {
-      Bucket: BUCKET_NAME,
-      Key: `${folder}/${new Date().toISOString()}.jpeg`,
-      Body: decodedFile,
-      ContentType: "image/jpeg",
+      Bucket: `${BUCKET_NAME}/${folder}`,
+      Key: filename,
+      Body: content,
+      ContentType: contentType
     };
-
     const uploadResult = await s3.upload(params).promise();
-
-    response.body = JSON.stringify({message: "Successfully uploaded file to S3", uploadResult});
+    body = JSON.stringify({message: "Successfully uploaded file to S3", uploadResult});
   } catch (e) {
     console.error(e);
-    response.body = JSON.stringify({message: "File failed to upload", errorMessage: e});
-    response.statusCode = 500;
+    body = JSON.stringify({message: "File failed to upload", errorMessage: e});
+    statusCode = 500;
   }
-
-  return response;
+  let headers = {
+    "Content-Type": contentType,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, OPTIONS"
+  };
+  return {statusCode, body, headers};
 }
